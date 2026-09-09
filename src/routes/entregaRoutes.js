38cfcb17e707d9e476e1b72151c47e23244f7725
larahcmd <lara_hoffmann@estudante.sesisenai.org.br>
2026-09-09 17:20:44 -0300

    Update entregaRoutes.js

--- src/routes/entregaRoutes.js
+++ src/routes/entregaRoutes.js
@@ -1,1 +1,21 @@
+const express = require("express");
 
+const router = express.Router();
+
+router.get("/entregas", (req, res) => {
+    res.send("Lista de entregas");
+});
+
+router.post("/entregas", (req, res) => {
+    res.send("Entrega cadastrada");
+});
+
+router.put("/entregas/:id", (req, res) => {
+    res.send("Entrega atualizada");
+});
+
+router.delete("/entregas/:id", (req, res) => {
+    res.send("Entrega excluída");
+});
+
+module.exports = router;
