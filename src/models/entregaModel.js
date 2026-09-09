e66b772b470546488aa9c281da8d299a91ba9fbf
larahcmd <lara_hoffmann@estudante.sesisenai.org.br>
2026-09-09 17:31:04 -0300

    Update entregaModel.js

--- src/models/entregaModel.js
+++ src/models/entregaModel.js
@@ -1,1 +1,11 @@
+const entregaModel = {
+    id_entrega: Number,
+    endereco_origem: String,
+    endereco_destino: String,
+    data_entrega: Date,
+    status: String,
+    id_motorista: Number,
+    id_veiculo: Number
+};
 
+module.exports = entregaModel;
