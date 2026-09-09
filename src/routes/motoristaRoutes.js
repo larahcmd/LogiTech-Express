087d811a052b65987c28b5349616c734b7bbc3ac
larahcmd <lara_hoffmann@estudante.sesisenai.org.br>
2026-09-09 17:18:26 -0300

    Update motoristaRoutes.js

--- src/routes/motoristaRoutes.js
+++ src/routes/motoristaRoutes.js
@@ -1,1 +1,21 @@
+const express = require("express");
 
+const router = express.Router();
+
+router.get("/motoristas", (req, res) => {
+    res.send("Lista de motoristas");
+});
+
+router.post("/motoristas", (req, res) => {
+    res.send("Motorista cadastrado");
+});
+
+router.put("/motoristas/:id", (req, res) => {
+    res.send("Motorista atualizado");
+});
+
+router.delete("/motoristas/:id", (req, res) => {
+    res.send("Motorista excluído");
+});
+
+module.exports = router;
