e0d0154df1b836d1e26d84b0805c141449c053b8
larahcmd <lara_hoffmann@estudante.sesisenai.org.br>
2026-09-09 17:26:04 -0300

    Update veiculoService.js

--- src/services/veiculoService.js
+++ src/services/veiculoService.js
@@ -1,1 +1,14 @@
+const veiculoRepository = require("../repositories/veiculoRepository");
 
+const cadastrarVeiculo = (dados) => {
+    return veiculoRepository.cadastrar(dados);
+};
+
+const listarVeiculos = () => {
+    return veiculoRepository.listar();
+};
+
+module.exports = {
+    cadastrarVeiculo,
+    listarVeiculos
+};
