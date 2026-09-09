01930fa1753d8d6127bca9d5b6959b597b52aa32
larahcmd <lara_hoffmann@estudante.sesisenai.org.br>
2026-09-09 17:21:54 -0300

    Update motoristaController.js

--- src/controllers/motoristaController.js
+++ src/controllers/motoristaController.js
@@ -1,1 +1,14 @@
+const motoristaService = require("../services/motoristaService");
 
+const cadastrarMotorista = (req, res) => {
+    res.send("Cadastro de motorista");
+};
+
+const listarMotoristas = (req, res) => {
+    res.send("Lista de motoristas");
+};
+
+module.exports = {
+    cadastrarMotorista,
+    listarMotoristas
+};
