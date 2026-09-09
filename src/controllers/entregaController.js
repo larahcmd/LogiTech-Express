51db24883af4ef0471d6878957b99b242168e90c
larahcmd <lara_hoffmann@estudante.sesisenai.org.br>
2026-09-09 17:23:24 -0300

    Update entregaController.js

--- src/controllers/entregaController.js
+++ src/controllers/entregaController.js
@@ -1,1 +1,14 @@
+const entregaService = require("../services/entregaService");
 
+const cadastrarEntrega = (req, res) => {
+    res.send("Cadastro de entrega");
+};
+
+const listarEntregas = (req, res) => {
+    res.send("Lista de entregas");
+};
+
+module.exports = {
+    cadastrarEntrega,
+    listarEntregas
+};
