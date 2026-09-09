481392661f47d96183f9fe73e4de989aa88fec74
larahcmd <lara_hoffmann@estudante.sesisenai.org.br>
2026-09-09 17:24:30 -0300

    Update motoristaService.js

--- src/services/motoristaService.js
+++ src/services/motoristaService.js
@@ -1,1 +1,14 @@
+const motoristaRepository = require("../repositories/motoristaRepository");
 
+const cadastrarMotorista = (dados) => {
+    return motoristaRepository.cadastrar(dados);
+};
+
+const listarMotoristas = () => {
+    return motoristaRepository.listar();
+};
+
+module.exports = {
+    cadastrarMotorista,
+    listarMotoristas
+};
