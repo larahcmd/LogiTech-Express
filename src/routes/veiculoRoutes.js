ef849d11152032bc08ca4278e4c1e8b481a24f32
larahcmd <lara_hoffmann@estudante.sesisenai.org.br>
2026-09-09 17:19:48 -0300

    Update veiculoRoutes.js

--- src/routes/veiculoRoutes.js
+++ src/routes/veiculoRoutes.js
@@ -1,1 +1,21 @@
+const express = require("express");
 
+const router = express.Router();
+
+router.get("/veiculos", (req, res) => {
+    res.send("Lista de veículos");
+});
+
+router.post("/veiculos", (req, res) => {
+    res.send("Veículo cadastrado");
+});
+
+router.put("/veiculos/:id", (req, res) => {
+    res.send("Veículo atualizado");
+});
+
+router.delete("/veiculos/:id", (req, res) => {
+    res.send("Veículo excluído");
+});
+
+module.exports = router;
