edcb537cedda7f70b0b3e15220e46fd2bc9359e2
larahcmd <lara_hoffmann@estudante.sesisenai.org.br>
2026-09-09 17:22:46 -0300

    Update veiculoController.js

--- src/controllers/veiculoController.js
+++ src/controllers/veiculoController.js
@@ -1,1 +1,14 @@
+const veiculoService = require("../services/veiculoService");
 
+const cadastrarVeiculo = (req, res) => {
+    res.send("Cadastro de veículo");
+};
+
+const listarVeiculos = (req, res) => {
+    res.send("Lista de veículos");
+};
+
+module.exports = {
+    cadastrarVeiculo,
+    listarVeiculos
+};
